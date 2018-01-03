29e511f46bd8f0f1ed54499b85430af6673ae5a4
newUser <31165297+iDecipher@users.noreply.github.com>
2018-01-03 14:04:15 +0800

    3

--- Kwok/menu.asm
+++ Kwok/menu.asm
@@ -1,46 +1,72 @@
 .model small
 .stack 100
 .data                                
-    ;LOGIN---------------------------
-    loginstr db "Enter pass code $"
-    
+    ;LOGIN---------------------------     
     ARRAY LABEL BYTE
     max db 20
     act db ?
-    pass db 20 dup("$")                 
+    pass db 20 dup("$")   
     
-    password db "eewhore"          
-    
-    errorstr db "Pass code doesn't match!$"  
-    
+    loginstr db "Enter pass code $"                       
+    password db "123"              
+    errorstr db "Pass code doesn't match!$"      
     buffer db 20 dup ("$")
 
     ;MENU ---------------------------
     menu db "Bank Loan Machine$"
     menu1 db "1. Loan$" 
     menu2 db "2. Exit Program$"  
-    string db "This is loan$"
-	  ;dos box doesnt recognise 1ch as enter        
+    string db "This is loan$"       
+                   
 	arrow db ?          ;this stores input of arrow  
-	enter db ?          ;this stores input of enter                           
-	row db 0Eh                             
+	enter db ?          ;this stores input of enter 	                          
+	row db 0Eh                                      
+	
+	;LOAN-------------------------------     
+    errStr db "Max Loan Amount is 15000$"   
+    maxloan db "15000"              
+    minloan db "01000"  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;<---
+    loanStr db "Confirm Amount? $"
+    contStr db "Continue ?$" 
+    yes db "Yes$"
+    no db "No$"      
+    zero db 5 dup (30h), "$"
+    
+    cash db 5 dup (20h), "$" 
+    YN db ?   
+    col db 25h              ;always point to no 
+    num db ?
+    ;INTEREST-TABLE-----------------------------
+    int_Table1 db "Interest Rate & Loan Duration$"
+    int_Table2 db "Loan Duration     1-3 years       4-6years        7-10 years$"
+    int_Table3 db "Interest Rate     30%             18%             12%$"
+    intStr db "Loan Amount Selected: $" ;cash here
+    intDur db "Enter Loan Duration: $"
+    
+    Duration db 2 dup (?)   ;loan duration for 2 bytes      
 	                               	                               
 .code
 main proc
 	mov ax, @data
 	mov ds, ax    
                      
-    call _login                    
-    call _menu 
+    call _login                     
+    call _menu
+     
 EXIT:                
     mov ah, 4ch
     int 21h                     
 main endp  
 
 cls proc
-    mov ax, 0003h
-    int 10h         
-    xor ax,ax
+    mov ah, 6
+    xor al, al
+    xor cx, cx
+    mov dx, 184Fh       ;end coord
+    mov bh, 0Ah         ;cyan bg, green text
+    int 10h    
+    
+    ;borders     
           
     ret
                    
@@ -53,24 +79,22 @@ print proc
              
 color proc
     mov ax, 0920h       ;color focus      
-    mov dl, 16h         ;col : 24d
-    mov cx, 0017
+    mov dl, 28h         ;col : 24d
     int 10h 
     
     ret
 
 cursor proc    
     mov ah, 2           ;next cursor
-    mov dl, 16h         ;col : 24d
     mov bh, 0  
     int 10h
     
     ret    
     
-center_screen proc
+ctscr proc
     mov ax, 0200h       ;set cursor to middle     
     mov bh, 0           ;page 0
-    mov dx, 0D16h       ;center
+    mov dx, 0D28h       ;center
     int 10h
  
     ret       
@@ -82,35 +106,45 @@ clstr proc
     mov cl, max
     rep movsb
     
-    ret
+    ret      
+ 
+clbyte proc
+L7:     
+    mov cash[si], 30h         ;clear input
+    inc si
         
+    loop L7  
+    
+    ret      
                     
 _login proc     
 L1:    
     call clstr
-    call cls
+    call cls   
     
-    mov dh, 0Dh
+    mov dx, 0D28h   
     call cursor                      
-                          
+                 
+    xor dx, dx                         
     lea dx, loginstr
     call print 
                  
-    mov dh, 0Eh
+    mov dx, 0E28h
     call cursor
     
-    mov bx, 00F0h
+    mov bx, 00A0h     
+    mov cx, 0017
     call color  
     
-    mov dh, 0Eh
+    mov dx, 0E28h
     call cursor
                  
-    mov ah, 0Ah           
+    mov ah, 0Ah        
     lea dx, ARRAY
-    int 21h  
+    int 21h      
              
     xor cx, cx
-    mov cx, 7       ;change this as well when you change password 
+    mov cx, 3       ;change this as well when you change password 
     mov si, 0          
 L2:
     mov bl, password[si]
@@ -123,33 +157,36 @@ L2:
     
     ret     
             
-_menu proc 
-    call center_screen
-    ;set background color here  
-             
+_menu proc   
+MENU_SCR:          
+    call cls
+    call ctscr          
+                                
     lea dx, menu        ;print menu
     call print    
                   
-    mov dh, row        ;set row to 14 
+    mov dh, row        ;set row to 14      
+    mov dl, 28h         ;col : 24d
     call cursor
                                         
-    mov bx, 00F0h      ;point to first option   
+    mov bx, 00A0h      ;point to first option  
+    mov cx, 0017 
     call color  
     
                             
 L3:              
-    mov dh, 0Eh
+    mov dx, 0E28h
     call cursor  
     
     lea dx, menu1
     call print 
                
-    mov dh, 0Fh              
+    mov dx, 0F28h              
     call cursor
     
     lea dx, menu2
     call print         
-                             ;add menu3 
+    ;add menu3 
             
     mov ah, 0
     int 16h   
@@ -157,7 +194,8 @@ L3:
     mov arrow, ah
     mov enter, al
            
-    mov dh, row
+    mov dh, row     
+    mov dl, 28h         ;col : 24d
     call cursor
                  
     mov al, arrow
@@ -169,77 +207,298 @@ L3:
     
     mov al, enter  
     cmp al, 0Dh         ;enter key 0dh
-    je EN         
+    je EN1         
   
 B1:                                                               
     loop L3  
       
     
 UP:           
-    mov bx, 000Fh      ;set color back to normal
+    mov bx, 000Ah      ;set color back to normal 
+    mov cx, 0017
     call color
     
-    dec row        ;E - 1 = D
+    dec row        
     
-    mov bl, 0Dh    ;D
+    mov bl, 0Dh    
     cmp row, bl
-    je U_RESET    ;D => F
+    je U_RESET    
 
 U_BACK:    
-    mov dh, row
+    mov dh, row     
+    mov dl, 28h     
     call cursor     
     
-    mov bx, 00F0h         
+    mov bx, 00A0h            
+    mov cx, 0017
     call color
     
     jmp B1      
               
 U_RESET:    
-    add row, 2         ;go back to bottom
-    jmp U_BACK     
-      
-      
+    add row, 2          ;go back to bottom
+    jmp U_BACK          
       
 DOWN:                        
-    mov bx, 000Fh   ;set color back to normal
+    mov bx, 000Ah       ;set color back to normal  
+    mov cx, 0017
     call color
             
     inc row        
     
-    mov bl, 10h    ;condition
+    mov bl, 10h         ;condition
     cmp row, bl
-    je D_RESET  ;reset cursor
+    je D_RESET          ;reset cursor
          
 D_BACK:    
-    mov dh, row
+    mov dh, row    
+    mov dl, 28h         
     call cursor    
                 
-    mov bx, 00F0h
+    mov bx, 00A0h            
+    mov cx, 0017
     call color                                      
 
     jmp B1 
           
 D_RESET:
     sub row, 2         ;go back to top
-    jmp D_BACK                  
-      
-;--------------------------------------------------------          
-
-EN:
-    cmp row, 0Eh
-    je LOAN_FUNC
-    ;cmp row, 0Fh         to be added more menu selection
-    jmp EXIT         
-           
-LOAN_FUNC:          ;you can move this to your function   
-    xor dx, dx
+    jmp D_BACK                         
     
-    mov ax, 0003   
-    int 10h
+    
+    
+                                                                   
+LOAN:                   ;you can move this to your function   
+    call Loan_Amount
+    jmp MENU_SCR   
+   
+EN1:
+    cmp row, 0Eh
+    je LOAN
+    ;cmp row, 0Fh         to be added more menu selection
+    ret            
+_menu endp      
 
-    lea dx, string
+            
+            
+            
+            
+            
+            
+            
+            
+Loan_Amount proc
+L_restart:     
+    call cls
+    call ctscr             
+    
+    lea dx, zero
     call print
     
-_menu endp
+    mov cx, 5
+    mov si, 0        
+    mov num, 2Ch
+L4:               
+    mov dx, 0D2Ch
+    call cursor  
+    
+	
+    mov ah, 0
+    int 16h     
                 
+    cmp al, 0Dh           ;end input
+    je L_enter    
+    cmp al, 30h
+    jb L_err
+    cmp al, 39h
+    ja L_err    
+                           
+    mov cash[si], al  
+    
+    mov dh, 0Dh
+    mov dl, num
+	call cursor   	
+	dec num	
+	lea dx, cash         ;not more than 15000
+	call print    	
+	cmp cx, 1
+    je LIMIT     
+R1:	
+	inc si   	
+	loop L4 	  
+	jmp L_enter 
+
+L_back1:               ;resume loop
+    mov cx, 1 
+    jmp R1	
+
+LIMIT:   
+    mov cx, 5   
+    mov si, 0
+L6:        
+    mov al, cash[si]
+    cmp al, maxloan[si]         ;compare 15000
+    jbe L_enter                     ;reset
+    loop L6
+        
+L_err:            
+    call Loan_error   
+    jmp L_back2    	      ; get yes and no    
+;------------------------------------------------  	
+L_enter:          
+    mov dx, 1122h          ;11:12
+    call cursor
+  
+    lea dx, loanstr       ;confirm amount
+    call print
+                  
+    mov ah, 1
+    int 21h
+ 
+    cmp al, "y"
+    call Loan_Duration    ;je ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;   
+L_right:          
+    mov dx, 141Fh          ;print yes coord - 1
+    call cursor
+    mov col, 25h 
+
+    mov bx, 000Ah
+    mov cx, 0005
+    call color  
+    
+L_back2:                  ;start coordinate first option
+    mov dx,1425h
+    call cursor
+    
+    mov bx, 00A0h      ;adjust coord
+    mov cx, 0004
+    call color
+    
+    jmp L_condition
+L_farjmp:
+    jmp L_restart                      
+    
+L_left:       
+    mov dx, 1425h
+    call cursor
+
+    mov bx, 000Ah      ;point to first option
+    mov cx, 0004   
+    call color
+    
+    mov col, 20h
+    
+    mov dx, 141Fh
+    call cursor
+    
+    mov bx, 00A0h  
+    mov cx, 0005
+    call color             ;change cx to 4 add own function
+
+L_condition:          
+    call Loan_condition
+    
+    mov ah, 0
+    int 16h    
+    
+    mov YN, ah           ; yes and no
+    mov enter, al       ;reuse enter variable
+    
+    cmp YN, 4Bh         ;YES
+    je L_left  
+    
+    cmp YN, 4Dh         ;NO
+    je L_right    
+    
+    cmp enter, 0Dh 
+    je L_end
+
+    loop L_condition          ;loop left and right  
+    
+L_end:             ;return    
+    cmp col, 20h
+    je L_farjmp    
+    
+    mov cx, 5
+    xor si, si
+    call clbyte
+                                                    
+    ret
+Loan_Amount endp               
+
+Loan_error proc
+    mov dx, 1021h               ;error display
+    call cursor
+ 
+    lea dx, errStr              
+    call print       
+              
+    mov cx, 5 
+    lea bx, cash 
+    call clbyte          ;clear byte
+    
+    mov dx, 1122h        ;print below 
+    call cursor
+    
+    lea dx, contStr       ;continue?
+    call print            
+    
+    ret
+
+Loan_condition proc
+    mov dx, 1420h         ;20h
+    call cursor         
+    
+    lea dx, yes
+    call print 
+    
+    mov dx, 1426h
+    call cursor
+    
+    lea dx, no
+    call print                  ;cursor for no
+    
+    ret
+
+
+Loan_Duration proc 
+    call cls
+    call ctscr    
+    
+    lea dx, int_Table1
+    call print        
+               
+    mov dx, 0E23h                     
+    call cursor
+    
+    lea dx, int_Table2
+    call print    
+    
+    mov dx, 0F23h                     
+    call cursor
+    
+    lea dx, int_Table3
+    call print    
+    
+    mov dx, 1025h                     
+    call cursor
+    
+    lea dx, intStr
+    call print          
+    
+    lea dx, cash    
+    call print    
+    
+    mov dx, 1125h                     
+    call cursor
+    
+    lea dx, intDur
+    call print
+               
+    mov ah, 0
+    int 16h              
+               
+    ret
+Loan_Duration endp           
+           
 end main
